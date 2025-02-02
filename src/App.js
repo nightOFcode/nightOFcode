7b69f6df8407ab1431ebbeb8e862976a7cc5979f
palzivar <a.h.shirazi1380.10@gmail.com>
2025-02-02 23:03:57 +0330

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 
+
 function App() {
   return (
     <div className="App">
       <Navbar />
     </div>
   );
+
 }
+
+
 
 export default App;
